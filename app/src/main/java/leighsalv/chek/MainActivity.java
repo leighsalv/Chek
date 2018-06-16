f9fa10f76b8517a1f8191b1ac22e968a49a3284e
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-06-15 22:50:17 -0700

    changed launch logo, added checkboxes

--- app/src/main/java/leighsalv/chek/MainActivity.java
+++ app/src/main/java/leighsalv/chek/MainActivity.java
@@ -2,12 +2,15 @@ package leighsalv.chek;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import java.util.regex.*;
 
 import java.util.ArrayList;
 
@@ -29,10 +32,17 @@ public class MainActivity extends AppCompatActivity {
         addBtn = findViewById(R.id.add_btn);
         listToDo = findViewById(R.id.list_todo);
 
+//        if(inputToDo.getText().toString().equals(""))
+//            addBtn.setClickable(false);
+//
+//        else if(!(inputToDo.getText().toString().equals("")))
+//            addBtn.setClickable(true);
+
         items = SaveItems.readData(this);
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, items);
         listToDo.setAdapter(adapter);
+
     }
 
     //Add or Remove button is pressed
@@ -40,11 +50,29 @@ public class MainActivity extends AppCompatActivity {
         switch(v.getId()) {
             case R.id.add_btn:
                 String item = inputToDo.getText().toString();
-                adapter.add(item);
-                inputToDo.setText("");
-                SaveItems.writeData(items, this);
-                Toast.makeText(this, "added", Toast.LENGTH_SHORT).show();
-                break;
+
+                if(item.length()!=0) {
+                    adapter.add(item);
+                    inputToDo.setText("");
+                    SaveItems.writeData(items, this);
+                    Toast.makeText(this, "added", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                else
+                    addBtn.setClickable(false);
         }
     }
+
+//    public void onClickToDel(View d) {
+//        CheckedTextView check = (CheckedTextView)d;
+//        check.setChecked(!check.isChecked());
+//        boolean click = !check.isChecked();
+//        check.setChecked(click);
+//        if(click) {
+//            Toast.makeText(this, "Not Selected", Toast.LENGTH_SHORT).show();
+//        }
+//        else {
+//            Toast.makeText(this, "Selected", Toast.LENGTH_SHORT).show();
+//        }
+//    }
 }
