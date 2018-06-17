771927c3a6cf800d9290d02ee750ee3bf2818748
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-06-16 23:58:47 -0700

    implemented delete task

--- app/src/main/java/leighsalv/chek/MainActivity.java
+++ app/src/main/java/leighsalv/chek/MainActivity.java
@@ -3,7 +3,9 @@ package leighsalv.chek;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
@@ -30,22 +32,34 @@ public class MainActivity extends AppCompatActivity {
 
         inputToDo = findViewById(R.id.input_todo);
         addBtn = findViewById(R.id.add_btn);
-        listToDo = findViewById(R.id.list_todo);
-
-//        if(inputToDo.getText().toString().equals(""))
-//            addBtn.setClickable(false);
-//
-//        else if(!(inputToDo.getText().toString().equals("")))
-//            addBtn.setClickable(true);
+        listToDo = (ListView)findViewById(R.id.list_todo);
 
         items = SaveItems.readData(this);
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, items);
         listToDo.setAdapter(adapter);
 
+        listToDo.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                SparseBooleanArray positionChecker = listToDo.getCheckedItemPositions();
+                int count = listToDo.getCount();
+
+                for(int i = count-1; i >= 0; i--) {
+                    if(positionChecker.get(i)) {
+                        adapter.remove(items.get(i));
+                    }
+                }
+                positionChecker.clear();
+                adapter.notifyDataSetChanged();
+
+                return false;
+            }
+        });
+
     }
 
-    //Add or Remove button is pressed
+    //Add To-Do Item
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.add_btn:
@@ -62,17 +76,4 @@ public class MainActivity extends AppCompatActivity {
                     addBtn.setClickable(false);
         }
     }
-
-//    public void onClickToDel(View d) {
-//        CheckedTextView check = (CheckedTextView)d;
-//        check.setChecked(!check.isChecked());
-//        boolean click = !check.isChecked();
-//        check.setChecked(click);
-//        if(click) {
-//            Toast.makeText(this, "Not Selected", Toast.LENGTH_SHORT).show();
-//        }
-//        else {
-//            Toast.makeText(this, "Selected", Toast.LENGTH_SHORT).show();
-//        }
-//    }
 }
